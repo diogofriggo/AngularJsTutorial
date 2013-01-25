3075c9686e312b6aefacf35806f6a445a8509a83
diogofriggo <diogofriggobr@gmail.com>
2013-01-25 16:43:28 -0200

    Step 5

--- app/assets/javascripts/controllers/phoneListController.js
+++ app/assets/javascripts/controllers/phoneListController.js
@@ -1,8 +1,7 @@
-function PhoneListController($scope) {
-  $scope.phones = [
-    { name: 'Nexus S', snippet: 'Fast just got faster with Nexus S.', age: 0 },
-    { name: 'Motorola XOOM™ with Wi-Fi', snippet: 'The Next, Next Generation tablet.', age: 1 },
-    { name: 'MOTOROLA XOOM™', snippet: 'The Next, Next Generation tablet.', age: 2 }
-  ]
+function PhoneListController($scope, $http) {
+  $http.get('../data/phones.json').success(function(data) {
+    $scope.phones = data.splice(0, 5);
+  });
   $scope.orderProp = 'age';
 }
+PhoneListController.$inject = ['$scope', '$http'];
--- app/assets/javascripts/testing/unit-tests/tests.js
+++ app/assets/javascripts/testing/unit-tests/tests.js
@@ -1,17 +1,33 @@
 describe('PhoneListController', function(){
-  var scope, ctrl;
+  var scope, ctrl, $httpBackend;
 
-  beforeEach(function() {
-    scope = {},
-    ctrl = new PhoneListController(scope);
-  });
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('../data/phones.json').
+      respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
-  it('should create "phones" model with 3 phones', function() {
-    expect(scope.phones.length).toBe(3);
+    scope = $rootScope.$new();
+    ctrl = $controller(PhoneListController, {$scope: scope});
+  }));
+
+  it('should create "phones" model with 2 phones', function() {
+    $httpBackend.flush();
+    expect(scope.phones.length).toBe(2);
   });
 
   it('should set the default value of orderProp model', function() {
     expect(scope.orderProp).toBe('age');
   });
 
+  it('should create "phones" model with 2 phones fetched from xhr', function() {
+    expect(scope.phones).toBeUndefined();
+    $httpBackend.flush();
+
+    expect(scope.phones).toEqual([{name: 'Nexus S'},
+      {name: 'Motorola DROID'}]);
+  });
+
+  it('should set the default value of orderProp model', function() {
+    expect(scope.orderProp).toBe('age');
+  });
 });

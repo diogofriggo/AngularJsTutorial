9003fac218df5a3aadc79601c51914fc5c0cd4c5
diogofriggo <diogofriggobr@gmail.com>
2013-01-25 15:58:51 -0200

    Step 3

--- assets/javascripts/testing/phoneListControllerSpec.js
+++
@@ -1,7 +0,0 @@
-describe('PhoneListController', function(){
-  it('should create "phones" model with 3 phones', function(){
-    var scope = {},
-        controller = new PhoneListController(scope);
-    expect(scope.phones.length).toBe(4);
-  })
-});

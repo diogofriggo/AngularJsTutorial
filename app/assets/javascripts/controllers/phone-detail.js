ee1945532f654d8267a3a10ca8eead7113cc3d69
diogofriggo <diogofriggobr@gmail.com>
2013-01-28 17:58:04 -0200

    Mid Step 8

--- app/assets/javascripts/controllers/phone-detail.js
+++ app/assets/javascripts/controllers/phone-detail.js
@@ -1,3 +1,5 @@
-function PhoneDetailController($scope, $routeParams) {
-  $scope.phoneId = $routeParams.phoneId;
+function PhoneDetailController($scope, $routeParams, $http) {
+  $http.get('../data/details/' + $routeParams.phoneId + '.json').success(function(data) {
+    $scope.phone = data;
+  });
 }
--- app/assets/javascripts/controllers/phone-list.js
+++ app/assets/javascripts/controllers/phone-list.js
@@ -1,6 +1,6 @@
 function PhoneListController($scope, $http) {
   $http.get('../data/phones.json').success(function(data) {
-    $scope.phones = data;//.splice(0, 5);
+    $scope.phones = data;
   });
   $scope.orderProp = 'age';
 }
